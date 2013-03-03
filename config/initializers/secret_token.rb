d1ade72b527b783977a6722f202d87c9ed9a3fc4
morishita <tomohiro.m0219@gmail.com>
2013-03-03 13:44:02 +0900

    railsをインストール

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PBC2013Rails4::Application.config.secret_token = '58060e318bd05f0fa8379fab23b0df27651ffa5a1832d6bf7da81011ccd2e50bdf414c3c58a99f36e7e73081bbabdb4e34d76ccb6018c243994e08eca16580d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PBC2013Rails4::Application.config.session_store :cookie_store, key: '_PBC2013-rails4_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PBC2013Rails4::Application.config.session_store :active_record_store
